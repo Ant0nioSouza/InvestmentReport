ef1679be5b88c790705618559bf2fc4f133eb9a8
Antonio Souza <antoniosouza.dev@gmail.com>
2021-10-15 15:38:05 -0300

    ReadCSV method and fixed bugs

--- src/main/java/br/wtf/souza/utils/CsvUtils.java
+++ src/main/java/br/wtf/souza/utils/CsvUtils.java
@@ -3,20 +3,37 @@ package br.wtf.souza.utils;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class CsvUtils {
 
-    private String filePath;
     private String line;
-    private BufferedReader reader;
+    private static BufferedReader reader;
+    private String row[];
+
 
     /*
-    * @p filePath - Path of the CSV file.
-    * Load the csv class.
+     * @p filePath - Path of the CSV file.
+     * Load the csv class.
      */
-    public void loadCSV(String filePath) throws FileNotFoundException {
-        this.filePath = filePath;
+    public static void loadCSV(String filePath) throws FileNotFoundException {
         reader = new BufferedReader(new FileReader(filePath));
+    }
+
+
+    public String[] readCSV() throws IOException {
+        try {
+            while ((line = reader.readLine()) != null) {
+                row = line.split(",");
+            }
+            return row;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return null;
+        } finally {
+            reader.close();
+        }
     }
 
 }
