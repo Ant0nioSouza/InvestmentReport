0f8df8b0204d951e8ec7f0f0d4b947029e5c587c
Antonio Souza <antoniosouza.dev@gmail.com>
2021-10-15 21:36:37 -0300

    Comment and cols change.

--- src/main/java/br/wtf/souza/utils/CsvUtils.java
+++ src/main/java/br/wtf/souza/utils/CsvUtils.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CsvUtils {
 
@@ -20,22 +22,25 @@ public class CsvUtils {
         reader = new BufferedReader(new FileReader(filePath));
     }
 
-
-    public String[] readCSV() throws IOException {
+    /*
+    * Get row by row and return it into a List
+     */
+    public List<String[]> readCSV() throws IOException {
+        List<String[]> rows;
+        rows = new ArrayList<String[]>();
         try {
-            System.out.println("---");
             while ((line = reader.readLine()) != null) {
                 row = line.split(";");
                 for (int i = 0; i < row.length; i++) {
-                    if ((i + 1) % 5 == 0) {
-                        // row[i] is one row of the csv.
+                    if ((i + 1) % 4 == 0) {
+                        rows.add(row);
                     }
                 }
             }
-            return row;
+            return rows;
         } catch (Exception e) {
             e.printStackTrace();
-
+            System.out.println(e);
             return null;
         } finally {
             reader.close();
